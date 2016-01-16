4579f921df2793ced49fd984e94f355af95e57a1
whitehaven <accrist@gmail.com>
2016-01-16 03:02:28 -0700

    spawn a circle using a simple pyglet primitive.

--- Spherodidact.py
+++ Spherodidact.py
@@ -1,5 +1,14 @@
 import pyglet
+import primitives
 
 window = pyglet.window.Window()
-window.clear()
+circle = primitives.Circle(x=200, y=200, z=0, width=50, color=(1, 0, 0, 1), stroke=25, rotation=0.0,
+                           style=pyglet.gl.GLU_FILL)
+
+
+@window.event
+def on_draw():
+    window.clear()
+    circle.render()
+
 pyglet.app.run()
