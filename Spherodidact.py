ff7a2e81e0f5949a6f864b27e65ac5bb7356f881
Alex Crist <accrist@gmail.com>
2016-03-03 01:25:31 -0700

    Discovered pyglet2d module, no need to reimplement.

--- Spherodidact.py
+++ Spherodidact.py
@@ -1,28 +1,22 @@
 import pyglet
-
-import primitives
+import pyglet2d
 
 frame_rate = 120.0
 
 window = pyglet.window.Window()
-circle = primitives.Circle(x=200, y=200, z=0, width=50, color=(1, 0, 0, 1), stroke=25, rotation=0.0,
-                           style=pyglet.gl.GLU_FILL)
-polygon = primitives.Polygon([(10, 10), (30, 10), (60, 60)], z=0, color=(0, 1, 0, 1), stroke=0, rotation=0.0,
-                             style=pyglet.gl.GLU_FILL)
 
+circle = pyglet2d.Shape.circle((300, 300), 50)
+
+circle.color = (0, 255, 0)
 
 @window.event
 def on_draw():
     window.clear()
-    circle.render()
-    polygon.render()
+    circle.draw()
 
 
 def update_positions(dt):
-    circle.x += 10 * dt
-    polygon.rotation += 10 * dt
-    polygon.x += 10 * dt
-    polygon.y += 10 * dt
+    circle.translate((1, 1))
 
 
 pyglet.clock.schedule_interval(update_positions, 1 / frame_rate)
